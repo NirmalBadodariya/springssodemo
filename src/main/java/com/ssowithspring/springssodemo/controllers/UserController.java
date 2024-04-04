c4980039981b3db9aeba25a383e482b07d0511db
UtsavDabhi16 <utsav@inexture.com>
2024-04-04 15:42:02 +0530

    updated code for the attribute name

--- src/main/java/com/ssowithspring/springssodemo/controllers/UserController.java
+++ src/main/java/com/ssowithspring/springssodemo/controllers/UserController.java
@@ -1,8 +1,16 @@
 package com.ssowithspring.springssodemo.controllers;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.hibernate.grammars.hql.HqlParser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class UserController {
@@ -13,7 +21,15 @@ public class UserController {
     }
 
     @GetMapping("/post-login-url")
-    public String postLogin() {
+    public String postLogin(HttpServletRequest request,Model model) {
+        Principal principal = request.getUserPrincipal();
+        if(((OAuth2AuthenticationToken) principal).getAuthorizedClientRegistrationId().equals("google")){
+            model.addAttribute("name", ((OAuth2AuthenticationToken) principal).getPrincipal().getAttribute("name"));
+        }else if(((OAuth2AuthenticationToken) principal).getAuthorizedClientRegistrationId().equals("github")){
+            model.addAttribute("name", ((OAuth2AuthenticationToken) principal).getPrincipal().getAttribute("login"));
+        }else  if (((OAuth2AuthenticationToken) principal).getAuthorizedClientRegistrationId().equals("facebook")){
+            model.addAttribute("name", ((OAuth2AuthenticationToken) principal).getPrincipal().getAttribute("name"));
+        }
         return "post-login";
     }
 
